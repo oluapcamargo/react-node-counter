ae9575738cd155458e4097f6bdef446ba64a77e3
paulo.camargo <paulo.camargo@globalsys.com.br>
2021-03-03 15:41:26 -0300

    Update project;

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+export function Profile(){
+    return(
+        <div>
+            <img src="https://github.com/oluapcamargo" alt="Paulo Henrique Camargo"/>
+            <div>
+                <strong>
+                    Paulo Henrique Camargo
+                </strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import React from "react";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+export default function Home() {
+  return (
+    <div className="container">
+    
+          <ExperienceBar/>
+          <section>
+            <div>
+              <Profile />
+            </div>
+            <div>
+              
+            </div>
+          </section>
+    </div>
+  )
+}
